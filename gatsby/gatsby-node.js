807df0ccb9fa4bcf46e3285d60e47637fde9f0c7
Michael Esteban <mickel13@gmail.com>
2020-11-08 12:25:30 +1000

    create individual slicemaster pages (32)

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -103,13 +103,24 @@ async function turnSlicemastersIntoPages({ graphql, actions }) {
       }
     }
   `);
-  // TODO: 2. Turn each slicemaster into their own page (TODO)
+
+  // 2. Turn each slicemaster into their own page
+  data.slicemasters.nodes.forEach((slicemaster) => {
+    actions.createPage({
+      component: path.resolve("./src/templates/Slicemaster.js"),
+      path: `/slicemaster/${slicemaster.slug.current}`,
+      context: {
+        name: slicemaster.person,
+        slug: slicemaster.slug.current,
+      },
+    });
+  });
+
   // 3. Figure out how many pages there are based on how many slicemasters there are, and how many per page!
   const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE);
   const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize);
   // 4. Loop from 1 to n and create the pages for them
   Array.from({ length: pageCount }).forEach((_, i) => {
-    console.log(`Creating page ${i}`);
     actions.createPage({
       path: `/slicemasters/${i + 1}`,
       component: path.resolve("./src/pages/slicemasters.js"),
